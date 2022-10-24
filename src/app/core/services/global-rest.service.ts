240a4d325018dab7f1801edba62ad27e71a79767
R4vage <jmarc@applaudostudios.dev>
2022-10-23 21:59:51 -0300

    fixing https for netlify

--- src/app/core/services/global-rest.service.ts
+++ src/app/core/services/global-rest.service.ts
@@ -1,31 +1,43 @@
-import { LoginResponse, GetProductsResponse, LoginData, RegisterResponse } from './../../models/rest.models';
+import {
+  LoginResponse,
+  GetProductsResponse,
+  LoginData,
+  RegisterResponse,
+} from './../../models/rest.models';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GlobalRestService {
-url = "http://sheltered-oasis-97086.herokuapp.com"
-httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
+  url = 'https://sheltered-oasis-97086.herokuapp.com';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
 
-  constructor(private http: HttpClient) {
-  }
-  refreshToken (token:string) {
-    return this.http.post<LoginResponse>(this.url + '/auth/refresh', {
-      refreshToken: token
-    }, this.httpOptions);
-  }
-
-  getProducts () {
-    return this.http.get<GetProductsResponse>(this.url + "/products")
-  }
-
-  loginUser (userData: LoginData) {
-    return this.http.post<LoginResponse>(this.url + "/auth/login", userData)
+  constructor(private http: HttpClient) {}
+  refreshToken(token: string) {
+    return this.http.post<LoginResponse>(
+      this.url + '/auth/refresh',
+      {
+        refreshToken: token,
+      },
+      this.httpOptions
+    );
   }
 
-  registerUser (userData: LoginData) {
-    return this.http.post<RegisterResponse>(this.url + "/auth/signup", userData, this.httpOptions)
+  getProducts() {
+    return this.http.get<GetProductsResponse>(this.url + '/products');
+  }
+
+  loginUser(userData: LoginData) {
+    return this.http.post<LoginResponse>(this.url + '/auth/login', userData);
+  }
+
+  registerUser(userData: LoginData) {
+    return this.http.post<RegisterResponse>(
+      this.url + '/auth/signup',
+      userData,
+      this.httpOptions
+    );
   }
 }
